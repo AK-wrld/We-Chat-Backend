b27787d6ecb322a69cad1fc157914309dbd06365
Apurba Koley <apurba@Apurbas-MacBook-Pro.local>
2024-03-11 00:07:51 +0530

    call backend implemented

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ io.on("connection", (socket) => {
         // console.log(data.uid," user joined room")
         socket.emit("joined_room",data)
     })
+
+    // friend related events
     socket.on("add_friend",(data)=> {
         // console.log('user joining room',data.friendId)
         io.in(data.friendId).emit("friend_request",data.message)
@@ -34,6 +36,8 @@ io.on("connection", (socket) => {
         console.log(data.uid)
         io.in(data.uid).emit("friend_req_unsent",data.message)
     })
+
+    //chat related events
     socket.on("send_message",(data)=> {
         console.log(data.message)
         io.in(data.uid).emit("add_message",data.message)
@@ -46,6 +50,31 @@ io.on("connection", (socket) => {
     socket.on("notTyping",(data)=> {
         // console.log(data.uid)
         io.in(data.uid).emit("not_typing")
+    })
+
+    // call related events
+    socket.on("callUser",(data)=> {
+        const {profileData} = data;
+        console.log("user called",data.userToCall)
+        // console.log(data.signalData)
+        io.in(data.userToCall).emit("incoming_call",{signalData:data.signalData,from:data.from,profileData})
+    })
+    socket.on("busyCall",(data)=> {
+        // console.log({data})
+        io.in(data.caller).emit("busy_call",data.me)
+    })
+    socket.on("rejectCall",(data)=> {
+        // console.log("call rejected",data.caller)
+        io.to(data.caller).emit("call_rejected",data.me)
+    })
+    socket.on("acceptCall", (data) => {
+        console.log("chala")
+        // console.log({data})
+        io.to(data.caller).emit('call_accepted', data.signalData);
+    })
+    socket.on("endCall",friendId=> {
+        console.log(friendId)
+        socket.in(friendId).emit("call_ended")
     })
   });
 httpServer.listen(80);
